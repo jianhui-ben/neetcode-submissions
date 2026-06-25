71f45788cc043e1cf21fb4ad3ff3b7812d4dd2d4
Jianhui Ben <56848520+jianhui-ben@users.noreply.github.com>
2026-06-25 00:00:05 -0700

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        """
+        using division is very easy
+
+        but we could also treat every output, 
+        res[i] = product of nums[0] to nums[i - 1] * nums[i + 1] to nums[-1]
+        for each res[i], we maintain two stacks:
+        left_to_right[i] = product up to index i from left to right
+        right_to_left[i] = product to index i from right to left
+
+        res[i] = left_to_right[i - 1] * right_to_left[i + 1]
+        
+        edge case:
+        i = 0: left_to_right should be 1
+        i = len(nums) - 1: right_to_left should be 1
+        so we could append one extra 1 to every head of the stack
+
+        time: O(n)
+        space: O(n)
+        """
+        left_to_right, right_to_left = [1], [1]
+        for i in range(len(nums)):
+            ## [1, 1, 2, 8, 48]
+            left_to_right.append(nums[i] * left_to_right[-1])
+        
+        for i in range(len(nums) - 1, -1, -1):
+            ## [1, 6, 24, 48, 48]
+            right_to_left.append(nums[i] * right_to_left[-1])
+
+        return [left_to_right[i] * right_to_left[len(nums) - i - 1] for i in range(len(nums))]
