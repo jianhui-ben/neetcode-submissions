5974a7942aa97733fa2a8f4156d402627cec7395
Jianhui Ben <56848520+jianhui-ben@users.noreply.github.com>
2026-07-19 10:23:19 -0700

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,26 @@
+from collections import Counter
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        """
+        maintain the substring into dict
+        """
+        if len(s1) > len(s2):
+            return False
+
+
+        left, right = 0, 0
+        cur_window = {}
+        s1_dict = dict(Counter(s1))
+
+        while right < len(s2):
+            cur_window[s2[right]] = cur_window.get(s2[right], 0) + 1
+            right += 1
+            
+            while right - left > len(s1):
+                cur_window[s2[left]] -= 1
+                if not cur_window[s2[left]]:
+                    cur_window.pop(s2[left])
+                left += 1
+            if cur_window == s1_dict:
+                return True
+        return False
